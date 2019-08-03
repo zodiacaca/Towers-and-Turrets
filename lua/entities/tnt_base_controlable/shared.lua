b132334f063670cfda6950f4597659bb6a5244be
zodiacaca <121059464@qq.com>
2019-08-03 11:49:56 +0800

    u

--- lua/entities/tnt_base_controlable/shared.lua
+++ lua/entities/tnt_base_controlable/shared.lua
@@ -1,33 +1,34 @@
 
 ENT.Type 			= "anim"
 ENT.Base 			= "tnt_base_attachable"
-ENT.PrintName	= "M60 (Vehicle)"
+ENT.PrintName	= "tnt_base_controlable"
 ENT.Category		= "Towers and Turrets"
 
-ENT.Spawnable			= true
-ENT.AdminSpawnable	= true
+ENT.Spawnable			= false
+ENT.AdminSpawnable	= false
 ENT.AutomaticFrameAdvance = true	-- Animations will look smooth
 ENT.AdminOnly = false
 ENT.DoNotDuplicate = false
 
-ENT.Turret = "tnt_ctrl_m60"
-ENT.TurretModel = "models/tnt/m60_turret.mdl"
+ENT.Turret = ""
+ENT.TurretModel = ""
 ENT.TurretIdleSound = nil
-ENT.TurretTurningSound = Sound("TNT_M60.Turning")
-ENT.TurretShootSound = Sound("tnt/m60/m60-1.wav")
+ENT.TurretTurningSound = Sound("TNT_Turret.Turning")
+ENT.TurretShootSound = ""
 ENT.ImpactExplosionSound = ""
-ENT.TurretReloadSound = Sound("tnt/m60/m60_reload1.wav")	-- Default level of the sound is 65
+ENT.TurretReloadSound = ""	-- Default level of the sound is 65
 
 ENT.AimAttachment = 1	-- Used for aiming and effects
 ENT.MuzzleScale = 1
-ENT.MuzzleLightScale = 0.2
+ENT.MuzzleLightScale = 1
 ENT.TracerType = "Tracer"
-ENT.TracerCount = 3
+ENT.TracerCount = 1
 
 ENT.ImpactParticle = nil
 ENT.ImpactEffect = nil
 ENT.ImpactScale = 0
 
+ENT.SettleAnim = false
 ENT.SettleAngleRandom = false
 ENT.CanReload = true	-- not used, only literal meaning here
 
@@ -38,22 +39,22 @@ ENT.BlastDamage = 0
 ENT.BlastRadius = 0
 ENT.DamageScale = 1
 ENT.Spread = 0.01
-ENT.ClipSize = 200
+ENT.ClipSize = 50
 ENT.TakeAmmoPerShoot = 1
-ENT.Cooldown = 0.1
+ENT.Cooldown = 1
 
-ENT.EjectEffect = "RifleShellEject"
-ENT.EjectOffset = -28
+ENT.EjectEffect = nil
+ENT.EjectOffset = 0
 
-ENT.AimYawBone = "Bone001"
-ENT.AimPitchBone = "Arm_02"	-- These two bones can be the same bone or different like this one
-ENT.AimHeight = 12	-- The height of the gun
+ENT.AimYawBone = ""
+ENT.AimPitchBone = ""	-- These two bones can be the same bone or different like this one
+ENT.AimHeight = 64	-- The height of the gun
 ENT.YawLimitLeft = 0
 ENT.YawLimitRight = 0
 ENT.PitchLimitUp = 275
 ENT.PitchLimitDown = 85
 ENT.ExistAngle = 90	-- It's 90 in this model
-ENT.RecoilBone = "bolt"
+ENT.RecoilBone = nil
 
 ENT.RotateSpeed = 3.5
 ENT.RotateSpeedRatio = 0.75
